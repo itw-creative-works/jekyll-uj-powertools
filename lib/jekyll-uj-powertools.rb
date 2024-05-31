f154a2121f8283903fe19330e7be232c2f509377
Ian <ian.wiedenman@gmail.com>
2024-05-31 03:42:07 -0700

    Update

--- lib/jekyll-uj-powertools.rb
+++ lib/jekyll-uj-powertools.rb
@@ -4,12 +4,26 @@ module Jekyll
   module UJPowertools
     # Strip ads from the input
     def uj_strip_ads(input)
-      input.gsub(/\s*<ad-unit>[\s\S]*?<\/ad-unit>\s*/m, '')
+      input
+        # Remove HTML <ad-units>
+        .gsub(/\s*<ad-unit>[\s\S]*?<\/ad-unit>\s*/m, '')
+        # Remove includes starting with "/master/modules/adunits/"
+        .gsub(/\s*\{% include \/master\/modules\/adunits\/.*? %\}\s*/m, '')
     end
 
     # Escape a string for use in JSON
+    # def uj_json_escape(value)
+    #   value
+    #     .gsub('\\', '\\\\')  # Escape backslashes
+    #     .gsub('"', '\"')     # Escape double quotes
+    #     .gsub("\b", '\\b')   # Escape backspace
+    #     .gsub("\f", '\\f')   # Escape formfeed
+    #     .gsub("\n", '\\n')   # Escape newline
+    #     .gsub("\r", '\\r')   # Escape carriage return
+    #     .gsub("\t", '\\t')   # Escape tab
+    # end
     def uj_json_escape(value)
-      value.gsub('\\', '\\\\').gsub('"', '\"')
+      value.to_json[1..-2]  # Convert to JSON and remove the surrounding quotes
     end
 
     # Increment a global counter that can be accessed from any page then return the new value
