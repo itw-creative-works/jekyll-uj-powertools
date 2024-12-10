6c9de15b2e47bcbea4b6678ca6b52393818f27f6
Ian <ian.wiedenman@gmail.com>
2024-12-09 21:57:23 -0800

    Add injection

--- jekyll-uj-powertools.gemspec
+++ jekyll-uj-powertools.gemspec
@@ -20,8 +20,8 @@ Gem::Specification.new do |spec|
 
   # Files
   spec.files = Dir["CODE_OF_CONDUCT.md", "README*.md", "LICENSE", "Rakefile", "*.gemspec", "Gemfile", "lib/**/*", "spec/**/*"]
-  spec.executables   = spec.files.grep(%r{^bin/}) { |f| File.basename(f) }
-  spec.test_files    = spec.files.grep(%r{^(test|spec|features)/})
+  spec.executables = spec.files.grep(%r{^bin/}) { |f| File.basename(f) }
+  spec.test_files = spec.files.grep(%r{^(test|spec|features)/})
   spec.require_paths = ["lib"]
 
   # Gem requires Jekyll to work
--- lib/jekyll-uj-powertools.rb
+++ lib/jekyll-uj-powertools.rb
@@ -48,6 +48,43 @@ module Jekyll
       input.split(' ').map(&:capitalize).join(' ')
     end
   end
+
+  class InjectData < Generator
+    safe true
+    priority :low
+
+    def generate(site)
+      # Process pages
+      site.pages.each do |page|
+        inject_data(page, site)
+      end
+
+      # Process documents in all collections
+      site.collections.each do |_, collection|
+        collection.docs.each do |document|
+          inject_data(document, site)
+        end
+      end
+    end
+
+    private
+
+    def inject_data(item, site)
+      # Inject a random number into the item's data
+      item.data['random_id'] = rand(100) # Random number between 0 and 99
+
+      return unless item.data['layout'] # Skip items without layouts
+
+      # Find the layout file by its name
+      layout_name = item.data['layout']
+      layout = site.layouts[layout_name]
+
+      if layout && layout.data
+        # Merge layout front matter into item's "layout_data"
+        item.data['layout_data'] = layout.data
+      end
+    end
+  end
 end
 
 Liquid::Template.register_filter(Jekyll::UJPowertools)
